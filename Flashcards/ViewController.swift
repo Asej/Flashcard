82a4bcd0f4247223eb4efebbc107a2cbbf250547
Anthony Edwards <anthonydwrds8@gmail.com>
2018-12-02 15:13:08 -0500

    Re-added files

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Anthony Edwards on 10/13/18.
+//  Copyright © 2018 Anthony Edwards. All rights reserved.
+//
+
+import UIKit
+
+struct Flashcard{
+    var question : String
+    var answer : String
+}
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var frontLabel: UIView!
+    @IBOutlet weak var fr: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    @IBAction func didTapPrev(_ sender: Any) {
+    }
+    @IBAction func didTapNext(_ sender: Any) {
+    }
+    @IBAction func Correct(_ sender: Any) {
+        frontLabel.backgroundColor = UIColor.green;
+    }
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        if(backLabel.isHidden == false){
+               backLabel.isHidden = true;
+        }else{
+               backLabel.isHidden = false;
+        }
+        
+    }
+    var flashcards=[Flashcard]();
+    var index = 0;
+
+    @IBAction func nextPage(_ sender: Any) {
+    }
+    func updateFlashCard(question: String, answer: String){
+<<<<<<< HEAD
+        let flashcard = Flashcard(question: question, answer: answer)
+        flashcards.append(flashcard)
+        fr.text = question
+        backLabel.text = answer
+    }
+   
+=======
+        fr.text = question
+        backLabel.text = answer
+    }
+    @IBAction func Correct(_ sender: Any) {
+        frontLabel.backgroundColor = UIColor.green;
+    }
+>>>>>>> dc45978db53858999f88816734b0b47d8aecee32
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        updateFlashCard(question: "What is the capital of Brasil", answer: "Brasília")
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardsController = self
+        
+        if(segue.identifier == "EditSegue"){
+            creationController.initialQuestion = backLabel.text!
+            creationController.initialAnswer = fr.text!
+        }
+    }
+<<<<<<< HEAD
+}
+=======
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        let creationController = navigationController.topViewController as! CreationViewController
+        
+        creationController.flashcardsController = self
+        
+        if(segue.identifier == "EditSegue"){
+            creationController.initialQuestion = fr.text!
+            creationController.initialAnswer = backLabel.text!
+        }
+    }
+>>>>>>> dc45978db53858999f88816734b0b47d8aecee32
+
+
+
